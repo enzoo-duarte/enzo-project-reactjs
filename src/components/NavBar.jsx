4da2cd36f3439c3520d967b063a72500a292c1a6
Enzo Duarte <enzoduarte2019@gmail.com>
2025-02-07 22:59:55 -0300

    correcciones en estructura y estilos

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,15 +3,19 @@ import ButtonComponent from './ButtonComponent';
 import BrandComponent from './BrandComponent';
 import CartWidget from './CartWidget';
 
-export default function NavBar(){
-    return (
-            <nav>
-                <BrandComponent/>
-                <ButtonComponent text='Indumentaria'/>
-                <ButtonComponent text='Calzado'/>
-                <ButtonComponent text='Accesorios'/>
-                <CartWidget/>
-            </nav>
-    )
-}
+export default function NavBar() {
+  return (
+    <nav className="navbar">
+      <div className="navbar-brand">
+        <BrandComponent />
+      </div>
 
+      <div className="navbar-buttons">
+        <ButtonComponent text="Indumentaria" />
+        <ButtonComponent text="Calzado" />
+        <ButtonComponent text="Accesorios" />
+        <CartWidget />
+      </div>
+    </nav>
+  );
+}
